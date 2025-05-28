236e6532474624ed7401546b60f0d9142361a004
An V <65511075+nivihelen99@users.noreply.github.com>
2025-05-28 10:54:28 -0700

    Update tcp.h

---
+++ NetFlow/include/netflow/packet/tcp.h
@@ -0,0 +1,14 @@
+#ifndef NETFLOW_PACKET_TCP_H_
+#define NETFLOW_PACKET_TCP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// C function declarations and C-compatible struct definitions will go here
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif  // NETFLOW_PACKET_TCP_H_
